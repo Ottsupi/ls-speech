a2cc5e5dc1952469b1e14d384db3cea1cef8e730
Regis <ott.regisorate@gmail.com>
2023-10-24 19:17:05 +0800

    Setup a separate database for testing the repository.

---
+++ src/test/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO speech(author, speech, keywords, date) VALUES
+    ("Java", "Java is platform-independent...", "programming language object-oriented", "2023-01-01"),
+    ("Python", "Python is a general-purpose...", "programming language dynamic types", "2010-01-01"),
+    ("C#", "C# supports multiple paradigms...", "programming language strong typing", "1990-01-01"),
+    ("delete", "entry to be deleted", "delete", "2010-01-01")
